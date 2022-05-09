696ad6b8a49a884d2f995d129d37208a58a59203
ShubhamSatnurkar <shubhamsatnurkar061@gmail.com>
2022-05-09 21:14:04 +0530

    responsive work completed..

--- app/js/slider-custom.js
+++ app/js/slider-custom.js
@@ -108,19 +108,19 @@ $('#spotlightSlider').slick({
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1 
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
       }
     ]
-  });
+});
 // Spotlight slider end
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass') (require('node-sass'));
-let browserSync = require('browser-sync').create();
-let useref = require('gulp-useref');
-let uglify = require('gulp-uglify');
-let gulpIf = require('gulp-if');
-let cssnano = require('gulp-cssnano');
+// let browserSync = require('browser-sync').create();
+// let useref = require('gulp-useref');
+// let uglify = require('gulp-uglify');
+// let gulpIf = require('gulp-if');
+// let cssnano = require('gulp-cssnano');
 
-// gulp.task('hello', function(){
-//     console.log('hello')
-// })
+gulp.task('hello', function(){
+    console.log('hello')
+})
 
 gulp.task('styles',  function(){
     return gulp.src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
+    // .pipe(browserSync.reload({
+    //     stream: true
+    // }))
 });
 
-gulp.task('browserSync', function(){
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
-});
+// gulp.task('browserSync', function(){
+//     browserSync.init({
+//         server: {
+//             baseDir: 'app'
+//         },
+//     })
+// });
 
-gulp.task('useref', function(){
-    return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('app/js/*.js', uglify()))
-    .pipe(gulp.dest('dist'))
-});
+// gulp.task('useref', function(){
+//     return gulp.src('app/*.html')
+//     .pipe(useref())
+//     .pipe(gulpIf('app/js/*.js', uglify()))
+//     .pipe(gulp.dest('dist'))
+// });
 
 
 
@@ -40,6 +40,6 @@ gulp.task('watch', function(){
     gulp.watch('scss/**/*.scss', gulp.series(['styles']));
     
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch('app/*.html', browserSync.reload); 
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+    // gulp.watch('app/*.html', browserSync.reload); 
+    // gulp.watch('app/js/**/*.js', browserSync.reload);
 })
